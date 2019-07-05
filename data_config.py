f5793090481c55750f516bf2967091ef812a5a06
XiaoHua <flower0313@163.com>
2019-07-05 16:50:06 +0800

    #1修复bug，仿多个ip #2全股票输出

--- data_config.py
+++ data_config.py
@@ -1,15 +1,19 @@
 
+import random
 class Http_Request():
-    headers={
-"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-"Accept-Language":" zh-CN,zh;q=0.9,en;q=0.8",
-"Cache-Control": "max-age=0",
-"Connection": "keep-alive",
-"Cookie": "qgqp_b_id=55443a383a2142c4e4e466da95538cc7; st_si=90641855213591; st_asi=delete; st_pvi=42443299630348; st_sp=2019-07-01%2018%3A14%3A55; st_inirUrl=; st_sn=11; st_psi=20190701220622893-113200301321-8492325806; HAList=a-sz-300362-%u5929%u7FD4%u73AF%u5883; em_hq_fls=js",
-"Host": "quote.eastmoney.com",
-"Upgrade-Insecure-Requests": "1",
-"User-Agent":" Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
-}
+    def get_header(self):
+        ip = ['121.31.159.197', '175.30.238.78', '124.202.247.110','223.150.185.153']
+        header= {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
+            'X-Forwarded-For': ip[random.randint(0, 3)]}
+        return header
+
+
+
+
+
+
+
 
 class Choose_detail:
     def judge_one(data):
--- get_stock_detail.py
+++ get_stock_detail.py
@@ -2,37 +2,54 @@ import json
 from urllib import request
 import ssl
 import urllib
-ssl._create_default_https_context = ssl._create_unverified_context
 import urllib.parse
 import data_config
 import re
+import unittest
+import datetime
+import threading
+import socket
+import time
+import urllib.error
+import random
+ssl._create_default_https_context = ssl._create_unverified_context
 
-#读取有多少个股票数
-# with open('stocks.json') as j:
-#     count = json.load(j)
-# print("股票数："+len(count))
 def get_detail(secid,code_number):
     #fltt是控制小数点的，secid是控制股票代号
     url ='http://push2.eastmoney.com/api/qt/stock/get?&fltt=2&fields=f43,f57,f58,f169,f46,f44,f51,f168,f47,f60,f45,f116,f117,f52,f50,f48,f167,f71,f49,f60,f137,f188,f105,f173,f186,f195,f196,f43,f197&' \
-         'secid='+str(secid)+'.'+str(code_number)+'&cb=jQuery18304467440863261962_1562051616309&_=1562051616982'
+         'secid='+str(secid)+'.'+str(code_number)
     code =json.loads(getHtml(url))
-    return data_config.Choose_detail.judge_one(code)
-
-
-# stock_details,temp=[],{}
+    #return data_config.Choose_detail.judge_one(code)
+    return code['f58']
 
 def getHtml(url):#获取个股详细字典
-    request = urllib.request.Request(url, headers=data_config.Http_Request.headers)
-    html = urllib.request.urlopen(request).read().decode('utf-8')
-    res = re.search(r"(:{1})+(\{.+\})", html).group()[1:-1]
+    time.sleep(0.1)
+    mm=data_config.Http_Request()
+    request = urllib.request.Request(url, headers=mm.get_header())
+    try:
+        html = urllib.request.urlopen(request)
+        res = re.search(r"(:{1})+(\{.+\})", html.read().decode('utf-8')).group()[1:-1]
+    except:
+        time.sleep(3)
+        print("----------------------------------------")
+        html = urllib.request.urlopen(request)
+        res = re.search(r"(:{1})+(\{.+\})", html.read().decode('utf-8')).group()[1:-1]
     return res
-
-
 # for k,v in data_config.Choose_detail.judge_one(code).items():
 #     print(k+"："+str(v))
 
 
 #输出所有股票代码
+# with open('stocks.json') as j:
+#     for sc in json.load(j):
+#         print(get_detail(sc['secid'],sc['stock_code']))
+
+code_num=0
+start= datetime.datetime.now()#开始时间
 with open('stocks.json') as j:
-    for sc in json.load(j):
-        print(get_detail(sc['secid'],sc['stock_code']))
+    for sc in json.load(j)[:30]:
+        code_num = code_num+1
+        #t = threading.Thread(target=get_detail, args=())
+        print(get_detail(sc['secid'],sc['stock_code'])+str(code_num))
+end=datetime.datetime.now()#结束时间
+print('运行时间: %s 秒'%(end-start))
