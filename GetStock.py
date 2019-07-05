397f665aec4098d820ccff5b03abb4dcf75d1527
XiaoHua <flower0313@163.com>
2019-07-05 16:54:13 +0800

    #1修复bug，仿多个ip #2全股票输出

--- GetStock.py
+++ GetStock.py
@@ -7,7 +7,7 @@ import urllib.parse
 import data_config
 
 #东方财富股票的get请求,network抓包
-detail_url = "http://64.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124001651461580406255_1561992580766&pn=1&pz=3754&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2&fields=f12,f14,f13&_=1561992580767"
+detail_url = "http://64.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124001651461580406255_1561992580766&pn=1&pz=3754&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2&fields=f2,f12,f14,f13&_=1561992580767"
 # 爬虫抓取网页函数
 def getHtml(url):
     request = urllib.request.Request(url, headers=data_config.Http_Request.headers)
@@ -20,13 +20,18 @@ def getHtml(url):
 stocks,temp = [],{}
 code =json.loads(getHtml(detail_url))
 for name in code:
-    temp["stock_code"]=name["f12"]#写入字典
-    temp["stock_name"]=name["f14"]#写入字典
-    temp["secid"]=name["f13"]#写入字典
-    stocks.append(temp.copy())#将字典存入列表以组成json，必须用copy()
-    temp.clear()#再清空，因为一次存一个
+    if(name["f2"]!="-"):#所有未退市的股票
+        temp["stock_code"]=name["f12"]#写入字典
+        temp["stock_name"]=name["f14"]#写入字典
+        temp["secid"]=name["f13"]#写入字典
+        stocks.append(temp.copy())#将字典存入列表以组成json，必须用copy()
+        temp.clear()#再清空，因为一次存一个
 #存入json文件
 with open("stocks.json",'w') as f:
     json.dump(stocks, f, ensure_ascii=False, indent=4)#第二个参数是防止中文乱码，第三个参数是排列
 
+#读取股票数
+with open('stocks.json') as j:
+    count = json.load(j)
+print(len(count))
 #fhandle = open("./baidu.html", "wb") 创建新的文件
--- hj.py
+++ hj.py
@@ -1,206 +1,28 @@
-#######################################
-#时间：2019-6-23
-#项目：不能当韭菜
-#开发者：肖华
-#######################################
-from collections import OrderedDict
-import json
-import matplotlib.pyplot as plt
-from random import randint
-from die import Die
-import pygal
-import csv
+import threading
 import time
-import pandas
-from datetime import datetime
-from pygal_maps_world.maps import COUNTRIES
-from operator import itemgetter
-import requests
-from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
-# names = ['xiao','flower','xixi']
-# names.insert(1,'hua') #指定位置插入元素
-# names.append('haha') #在尾部插入元素
-# print(names)
-# del names[-1] #删除语句
-# print(names)
-# namesz = names.pop(1) #删除指定元素并赋值
-# print(namesz)
-# names.remove('xiao') #根据元素名删除指定元素
-# print(names)
-# names.sort(reverse=0) #按照字母序列排序打印
-# print(names)
-# names.reverse() #反转打印
-# len(names) #获得列表的长度
-# for name in names:  #for循环，记得冒号和缩进
-#     print("名字："+name)
-# numbers = list(range(2,11,2)) #从2开始每个加2，直到>=11结束并转换为列表
+import threading
+import time
+from queue import Queue
+import random
 
-# squares = [value**2 for value in range(1,11)]
-# print(squares)
-# print(squares[:])
-# numbers = (200,5,11)
-# print(numbers[0])
-# if (4 not in squares):
-#     print("Bingo!")
-# else:
-#     print("Bad")
+
+# def sayhi(num):  #定义每个线程要运行的函数
+#     print('running on number'+str(num)+'\n')
+#     time.sleep(3)
 #
-# alien_0={'color':'red','point':5} #字典,相当于Json数据
-# alien_0['x_position']=0
-# alien_0['y_position']=25
-# for k in alien_0:
-#     print(str(k))
-
-
-class Dog():
-    def __init__(self,name,age):
-        self.name2 = name
-        self.age3 = age
-    def sit(self,one):
-        print(str(one))
-
-favorite_languages = {
-    'jen':['python','ruby'],
-    'sarah':['c'],
-    'edward':['ruby','go'],
-    'phil':['python','haskell']
-}
-# def greet_user(num): #形参前面加一个*是空元组，**是一个空字典
-#     if (num==1 or num==2):
-#         return 1
-#     else:
-#         return greet_user(num-1)+greet_user(num-2)
-# print(greet_user(6))
-
-# test = OrderedDict() #调用别的类，类似于字典
-# test['name'] = 'flower'
-# test['age'] = 14
-# for k,v in test.items():
-#     print(k+'和'+str(v))
+# t1 = threading.Thread(target=sayhi,args=(33,),name="t1") #生成一个线程实例
+# t2 = threading.Thread(target=sayhi,args=(22,),name="t2") #生成另一个线程实例
 #
-# x = randint(1,6)
-class RnadomWalk():
-    def __init__(self,num_points=5000):
-        self.num_points=num_points
-        self.x_v=[0]
-        self.y_v=[0]
+# t1.start()  #启动线程
+# t2.start()
+# print("t1线程名："+t1.getName()) #获取线程名
+# print("t2线程名："+t2.getName())
+# t1.join()  #阻塞主线程，等待t1子线程执行完后再执行后面的代码
+# t2.join()  #阻塞主线程，等待t2子线程执行完后再执行后面的代码
+# print('-----end')
 
-    def fill_walk(self):
-        while len(self.x_v)<self.num_points:
-            x_dir = random.choice([1,-1])
-            x_dis = random.choice([0,1,2,3,4])
-            x_step = x_dis * x_dir
-
-            y_dir = random.choice([1, -1])
-            y_dis = random.choice([0, 1, 2, 3, 4])
-            y_step = y_dis * y_dir
-
-            if x_step==0 and y_step==0:continue
-
-            next_x = self.x_v[-1]+x_step
-            next_y = self.y_v[-1]+y_step
-
-            self.x_v.append(next_x)
-            self.y_v.append(next_y)
-
-# rw = RnadomWalk(50000)
-# rw.fill_walk()
-# point_numbers = list(range(rw.num_points))
-# plt.scatter(rw.x_v, rw.y_v,c=point_numbers,cmap=plt.cm.Blues,edgecolors='none',s=1)
-# plt.scatter(0,0,c='yellow',edgecolors='none',s=100) #起点
-# plt.scatter(rw.x_v[-1],rw.y_v[-1],c='red',edgecolors='none',s=100) #终点
-# plt.xticks([])
-# plt.yticks([])
-# plt.show()
-
-# die=Die()
-# results = []
-# for roll_num in range(1000):
-#     result = die.roll()
-#     results.append(result)
-#
-# frequencies = []
-# for value in range(1,die.num_sides+1):
-#     frequency=results.count(value)
-#     frequencies.append(frequency)
-#
-# hist= pygal.Bar()
-# hist.x_labels = [x for x in range(1,7)]
-# hist.add('D6',frequencies)
-# hist.render_to_file('die_visual.svg')
-
-# filename = 'sitka_weather_2014.csv'
-# with open(filename) as f:
-#     reader = csv.reader(f)
-#     header_row = next(reader)
-#
-#     highs,lows,dates=[],[],[]
-#     for row in reader:
-#         dates.append(datetime.strptime(row[0],"%Y/%m/%d"))
-#         highs.append(int(row[1]))
-#         lows.append(int(row[3]))
-# fig = plt.figure(dpi=128,figsize=(10,6))
-# plt.plot(dates,highs,c='red')
-# plt.plot(dates,lows,c='blue')
-# plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.1)
-# fig.autofmt_xdate()
-# plt.tick_params(axis='both',which='major',labelsize=16)
-# plt.show()
-
-#世界地图
-# def grt_country_code(c_name):
-#     for code,name in COUNTRIES.items():
-#         if(c_name==name):
-#             return code
-#     return None
-#
-# cc_pop = {}
-# filename='population_data.json'
-# with open(filename) as f:
-#     pop_data = json.load(f)
-# for pp in pop_data:
-#     if pp['Year'] =='2010':
-#         c_name = pp['Country Name']
-#         poplution = int(float(pp['Value']))
-#         code = grt_country_code(c_name)
-#         if code:
-#             cc_pop[code]=poplution
-# cc1,cc2,cc3={},{},{}
-# for cc,pop in cc_pop.items():
-#     if pop<10000000:
-#         cc1[cc]=pop
-#     elif pop<1000000000:
-#         cc2[cc]=pop
-#     else:
-#         cc3[cc]=pop
-# wm = pygal.maps.world.World()
-# wm.add('one', cc1)
-# wm.add('two', cc2)
-# wm.add('three', cc3)
-# wm.render_to_file('amer.svg')
-
-#17章API
-# url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
-# r=requests.get(url)
-# print("Staus code:",r.status_code)
-# response_dict = r.json()
-#
-# repo_ds = response_dict['items']
-# names,stars=[],[]
-# for rep in repo_ds:
-#     names.append(rep['name'])
-#     plot_dict={
-#         'value':rep['stargazers_count'],
-#         'label':rep['description'],
-#         'xlink':rep['html_url'],
-#     }
-#     stars.append(plot_dict)
-#
-# #可视化
-# my_style = LS('#333366',base_style=LCS)
-# chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
-# chart.x_labels=names
-# chart.add('',stars)
-# chart.render_to_file('pppp.svg')
-
-all_stock = "http://quote.eastmoney.com/stocklist.html"
+ip = ['121.31.159.197', '175.30.238.78', '124.202.247.110']
+headerss = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
+    'X-Forwarded-For': ip[random.randint(0, 2)]}
+print(headerss)
