7802a22cdf71b654b004057152b021b1977cba2e
XiaoHua <flower0313@163.com>
2019-07-08 11:02:00 +0800

    #1增加多线程抓取 #2更改循环抓取异常算法 #3将抓取时间12分钟缩短为1分钟

--- data_config.py
+++ data_config.py
@@ -2,7 +2,7 @@
 import random
 class Http_Request():
     def get_header(self):
-        ip = ['121.31.159.197', '175.30.238.78', '124.202.247.110','223.150.185.153']
+        ip = ['121.31.159.197', '175.30.238.78', '124.202.247.110','223.150.185.153','36.157.124.115','36.157.124.116','223.151.89.130']
         header= {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
             'X-Forwarded-For': ip[random.randint(0, 3)]}
