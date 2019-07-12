bff3ba4294a582f6c540c392e2d9c8cbbd8f423e
XiaoHua <flower0313@163.com>
2019-07-12 12:08:23 +0800

    #1完美解决筛选历史最高股bug #2是因为用了global变量，导致多线程影响

--- main_project/GetStock.py
+++ main_project/GetStock.py
@@ -3,14 +3,14 @@ import ssl
 import json
 import urllib
 import urllib.parse
-import data_config
+import project_files.data_config as d
 ssl._create_default_https_context = ssl._create_unverified_context
 
 #东方财富股票的get请求,network抓包
 detail_url = "http://64.push2.eastmoney.com/api/qt/clist/get?cb=jQuery1124001651461580406255_1561992580766&pn=1&pz=3754&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:0+t:6,m:0+t:13,m:0+t:80,m:1+t:2&fields=f2,f12,f14,f13&_=1561992580767"
 # 爬虫抓取网页函数
 def getHtml(url):
-    mm=data_config.Http_Request()
+    mm=d.Http_Request()
     request = urllib.request.Request(url, headers=mm.get_header())
     html = urllib.request.urlopen(request).read().decode('utf-8')
     pos_start = html.find("[")#截取以'['开头的
@@ -33,7 +33,7 @@ for name in code:
         temp["stock_code"]=name["f12"]#写入字典
         temp["stock_name"]=name["f14"]#写入字典
         temp["secid"]=name["f13"]#写入字典
-        if (str(name["f12"])[:3] == "300" or str(name["f12"])[:3] == "002" or str(name["f12"])[:3] == "000"):
+        if (str(name["f12"])[:3] == "300" or str(name["f12"])[:3] == "002" or str(name["f12"])[:3] == "000" or str(name["f12"][:3])=="001"):
             temp["mk"] = 2  # 看个股月线时有用：2代表深圳，1代表上海
         else:
             temp["mk"] = 1
@@ -44,7 +44,7 @@ with open("stocks.json",'w') as f:
     json.dump(stocks, f, ensure_ascii=False, indent=4)#第二个参数是防止中文乱码，第三个参数是排列
 
 #读取股票数
-with open('project_file/stocks.json') as j:
+with open('stocks.json') as j:
     count = json.load(j)
 print(len(count))
 #fhandle = open("./baidu.html", "wb") 创建新的文件
---
+++ main_project/Get_Newhigh.py
@@ -0,0 +1,78 @@
+import threading
+import time
+from queue import Queue
+from urllib import request
+import ssl
+import json
+import urllib.parse
+import project_files.data_config as d
+import datetime
+import re
+import urllib
+
+ssl._create_default_https_context = ssl._create_unverified_context
+lock=threading.Lock()
+def getHtml(name,code,mk,qq):#获取每个股票的月线详情
+    Stock_Year_Detail_Url = "http://pdfm.eastmoney.com/EM_UBG_PDTI_Fast/api/js?rtntype=5&token=4f1862fc3b5e77c150a2b985b12db0fd&" \
+                            "cb=jQuery18306381636561594661_1562731154930&id=" + code + mk + "&type=mk&authorityType=&_=1562731160889"
+    mm = d.Http_Request()
+    request = urllib.request.Request(Stock_Year_Detail_Url, headers=mm.get_header())
+    while True:
+        try:
+            html = urllib.request.urlopen(request, timeout=None)
+            data = re.search(r"(\[.+\])", html.read().decode('utf-8')).group()
+            break
+        except Exception as e:
+            time.sleep(3)
+            # if(str(e)=="'NoneType' object has no attribute 'group'"):
+            #     break
+            print("-----------------1"+name+"1------------------")
+            pass
+    #print(name+code+"\\"+str(temp_data))
+    qq.put(name+code+Compare_stock(data))
+    #urllib.error.URLError
+    #socket.timeout: timed out
+    #AttributeError
+#开始函数
+def List_Stocks():#先条用getHtml()再条用Compare_stock()
+    num = 0
+    results = []
+    with open("stocks.json","r") as j:
+        q = Queue()
+        threads = []
+        for sc in json.load(j):
+            num=num+1
+            t1=threading.Thread(target=getHtml,args=(sc["stock_name"],sc["stock_code"],str(sc["mk"]),q),)
+            t1.start()
+            threads.append(t1)
+        for _ in threads:
+            t1.join()
+        print("------以下为创历史新高的股票------")
+        for _ in threads:
+            #print(q.get(block=True,timeout=None)+"\\"+str(s))
+            results.append(q.get(block=True,timeout=None))
+    for s in results:
+         if(str(s)[-1]=="1"):
+             print(str(s)[:-1])
+    print("--------总计算量：%d--------"%num)
+
+#筛选出是否创新高
+def Compare_stock(data):#比较股票是否创新高
+     
+    Now_data=json.loads(data)
+    now_price = Now_data[-1].split(',')[2]
+    for line in Now_data:
+        #print(line.split(',')[2])
+        if(float(line.split(',')[2])>float(now_price)):
+            return "0"
+        else:
+            continue
+    return "1"
+
+start= datetime.datetime.now()#开始时间
+List_Stocks()
+#getHtml("松花","603863","1")
+end=datetime.datetime.now()#结束时间
+print('--运行时间: %s秒--'%(end-start))
+
+
