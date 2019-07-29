53ca817ea0130b8e74a383781a2a83518a4c6658
XiaoHua <flower0313@163.com>
2019-07-29 16:32:17 +0800

    #1增加DMI算法，有待考究

--- main_project/newtest.py
+++ main_project/newtest.py
@@ -14,8 +14,8 @@ volume = df.volume
 
 df.rename(columns={'close': 'Close', 'volume': 'Volume'}, inplace=True)
 
-sig_k , sig_d  = talib.STOCH(np.array(highPrice), np.array(lowPrice),
-							 np.array(close), fastk_period=9,slowk_period=3,
-							 slowk_matype=0, slowd_period=3, slowd_matype=0)
-sig_j = sig_k * 3 - sig_d  * 2
-sig = pd.concat([sig_k, sig_d, sig_j], axis=1, keys=['K', 'D', 'J'])
+# sig_k , sig_d  = talib.STOCH(np.array(highPrice), np.array(lowPrice),
+# 							 np.array(close), fastk_period=9,slowk_period=3,
+# 							 slowk_matype=0, slowd_period=3, slowd_matype=0)
+# sig_j = sig_k * 3 - sig_d  * 2
+# sig = pd.concat([sig_k, sig_d, sig_j], axis=1, keys=['K', 'D', 'J'])
